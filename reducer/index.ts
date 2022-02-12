f257be3a542a5a759b7a438a90b1ec54dcd2c0ce
Park <pjane123@naver.com>
2022-02-12 22:53:00 +0900

    Add reducer

---
+++ reducer/index.ts
@@ -0,0 +1,54 @@
+import { getRandomInteger, getRandomPairColor } from "../lib/index";
+
+type State = {
+  isPlaying: boolean;
+  stage: number;
+  score: number;
+  time: number;
+  answer: number;
+  defaultColor: string;
+  answerColor: string;
+};
+
+type Action =
+  | { type: "START_STAGE"; time: number }
+  | { type: "CLEAR_STAGE" }
+  | { type: "TIME_DECREASE"; time: number }
+  | { type: "GAME_OVER" };
+
+const initialState: State = {
+  isPlaying: false,
+  stage: 1,
+  score: 0,
+  time: 15,
+  answer: 0,
+  defaultColor: "",
+  answerColor: "",
+};
+
+const gameReducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "START_STAGE":
+      const blockCount = Math.pow(Math.round((state.stage + 0.5) / 2) + 1, 2);
+      const answerIndex = getRandomInteger(0, blockCount - 1);
+      const { defaultColor, answerColor } = getRandomPairColor(1);
+      return {
+        ...state,
+        isPlaying: true,
+        answer: answerIndex,
+        time: action.time,
+        defaultColor: defaultColor,
+        answerColor: answerColor,
+      };
+    case "CLEAR_STAGE":
+      return { ...state, stage: state.stage + 1 };
+    case "TIME_DECREASE":
+      return { ...state, time: state.time < action.time ? 0 : state.time - action.time };
+    case "GAME_OVER":
+      return { ...initialState };
+    default:
+      throw new Error("Action type error");
+  }
+};
+
+export { gameReducer, initialState };
