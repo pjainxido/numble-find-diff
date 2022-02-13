7ea816b6a02ab6250ec42df32300addbc93a5a43
Park <pjane123@naver.com>
2022-02-13 18:43:31 +0900

    Update index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -8,7 +8,7 @@ function plusOrMinus() {
 
 function getRandomPairColor(stage: number) {
   const baseRgb: number[] = Array.from({ length: 3 }, () => getRandomInteger(0, 255));
-  const matchDiff = Math.round(stage*0.2 + 0.5 / 2);
+  const matchDiff = Math.round(stage * 0.2 + 0.5 / 2);
   const answerRgb: number[] = baseRgb.map((item) => {
     return item + plusOrMinus() * Math.floor(item / matchDiff);
   });
