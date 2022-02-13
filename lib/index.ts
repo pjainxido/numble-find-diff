6b9fad34d6dc77832fb9667ee53fdef5ef62e688
Park <pjane123@naver.com>
2022-02-13 19:00:50 +0900

    Fix getRandomPairColor

--- lib/index.ts
+++ lib/index.ts
@@ -8,14 +8,12 @@ function plusOrMinus() {
 
 function getRandomPairColor(stage: number) {
   const baseRgb: number[] = Array.from({ length: 3 }, () => getRandomInteger(0, 255));
-  const matchDiff = Math.round(stage * 0.2 + 0.5 / 2);
+  const matchDiff = Math.round(stage / 3) + 0.2;
   const answerRgb: number[] = baseRgb.map((item) => {
     return item + plusOrMinus() * Math.floor(item / matchDiff);
   });
-
   const defaultColor: string = `rgb(${baseRgb[0]},${baseRgb[0]},${baseRgb[2]})`;
   const answerColor: string = `rgb(${answerRgb[0]},${answerRgb[0]},${answerRgb[2]})`;
-
   return { defaultColor, answerColor };
 }
 
